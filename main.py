625c6ac9245586d9904cd5953d92d1fa1507862d
Mount-Blanc <mountblanc100@gmail.com>
2023-06-15 14:46:31 -0700

    finished todo

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template, request, redirect
+
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+tasks = ["Task 1", "Task 2", "Task 3"]
+
+@app.route('/')
+def index():
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+    tasks.append(task)
+    return redirect('/')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    task = request.form['task']
+    tasks.remove(task)
+    return redirect('/')
